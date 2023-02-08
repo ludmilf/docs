ea86ec1bdebbdd6e8dd2d633eec0ca38ab317b72
Ludmil Filkov <ludmilf@gmail.com>
2023-02-08 22:55:15 +0200

    Add files via upload

---
+++ 20211016-plotly.py
@@ -0,0 +1,100 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 9,
+   "id": "2869b3a8",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# импортиране на необходими модули\n",
+    "import plotly\n",
+    "from plotly import __version__\n",
+    "# from plotly.offline import dounload_plotlyjs, init_notebook_mode, plot, iplot\n",
+    "from plotly.offline import init_notebook_mode, plot, iplot"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 10,
+   "id": "356af9aa",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/html": [
+       "        <script type=\"text/javascript\">\n",
+       "        window.PlotlyConfig = {MathJaxConfig: 'local'};\n",
+       "        if (window.MathJax) {MathJax.Hub.Config({SVG: {font: \"STIX-Web\"}});}\n",
+       "        if (typeof require !== 'undefined') {\n",
+       "        require.undef(\"plotly\");\n",
+       "        requirejs.config({\n",
+       "            paths: {\n",
+       "                'plotly': ['https://cdn.plot.ly/plotly-2.4.2.min']\n",
+       "            }\n",
+       "        });\n",
+       "        require(['plotly'], function(Plotly) {\n",
+       "            window._Plotly = Plotly;\n",
+       "        });\n",
+       "        }\n",
+       "        </script>\n",
+       "        "
+      ]
+     },
+     "metadata": {},
+     "output_type": "display_data"
+    }
+   ],
+   "source": [
+    "# Jupyter setup\n",
+    "init_notebook_mode(connected=True)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "38244fe4",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.10"
+  },
+  "vp": {
+   "base_numbering": 1,
+   "nav_menu": {},
+   "number_sections": true,
+   "sideBar": true,
+   "title_cell": "VisualPython",
+   "title_sidebar": "VisualPython",
+   "vpPosition": {
+    "height": "calc(100% - 180px)",
+    "right": "10px",
+    "top": "110px",
+    "width": "50%"
+   },
+   "vp_cell": false,
+   "vp_section_display": true,
+   "vp_window_display": false
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
